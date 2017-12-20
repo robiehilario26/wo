d84d6bf1c70877a6bf3359929b9939f6c0c5c0a8
carlthegreat <carlthegreat@carlthegreat-PC>
2017-12-20 15:37:24 +0800

    teasdsd

---
+++ src/java/com/wo/controller/LoginController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wo.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author carlthegreat
+ */
+@WebServlet(name = "LoginController", urlPatterns = {"/LoginController"})
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("testmail");
+
+        System.out.println("email test " + email);
+        
+    }
+
+}
---
+++ web/parent.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : parent
+    Created on : Dec 20, 2017, 2:58:04 PM
+    Author     : carlthegreat
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="refresh" content="0;url=pages/test.jsp">
+<title>SB Admin 2</title>
+<script language="javascript">
+    window.location.href = "pages/test.jsp"
+</script>
+    </head>
+    <body>
+       Go to <a href="pages/test.jsp">/pages/index.test</a>
+    </body>
+</html>
